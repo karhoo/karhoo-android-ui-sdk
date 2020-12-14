4131a338fb4f68c7a2e3ad30d96358887f25d5d2
Jo Santa Maria <jo.santamaria@karhoo.com>
2020-12-14 10:47:12 +0000

    Enable cancel button on error

--- uisdk/src/main/java/com/karhoo/uisdk/screen/booking/booking/bookingrequest/BookingRequestView.kt
+++ uisdk/src/main/java/com/karhoo/uisdk/screen/booking/booking/bookingrequest/BookingRequestView.kt
@@ -94,7 +94,6 @@ class BookingRequestView @JvmOverloads constructor(context: Context,
         isGuest = isGuest()
         attachListeners()
         bookingRequestFlightDetailsWidget.setHintText(context.getString(R.string.add_flight_details))
-        val isValid = bookingRequestPassengerDetailsWidget.allFieldsValid()
         presenter.setBookingFields(bookingRequestPassengerDetailsWidget.allFieldsValid())
         TextViewCompat.setTextAppearance(bookingRequestLabel, R.style.ButtonText)
     }
@@ -267,6 +266,7 @@ class BookingRequestView @JvmOverloads constructor(context: Context,
 
     override fun onError() {
         bookingRequestButton.onLoadingComplete()
+        cancelButton.isEnabled = true
     }
 
     override fun resetBookingButton() {
