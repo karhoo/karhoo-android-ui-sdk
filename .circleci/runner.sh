eef83f3855444fdb81fd2459c5cc1b7e9a8c553b
Jo Santa Maria <60882714+jsm20@users.noreply.github.com>
2020-12-17 14:43:53 +0000

    Remove include test

--- .circleci/runner.sh
+++ .circleci/runner.sh
@@ -3,7 +3,6 @@
    -u "$SAUCE_USERNAME" \
    -k "$SAUCE_ACCESS_KEY" \
    -f espresso \
-   --include-tests='class com.karhoo.uisdk.address.AddressTests' \
    --data-center eu-central-1 \
    -a ../app/build/outputs/apk/debug/app-debug.apk \
    -t ../app/build/outputs/apk/androidTest/debug/app-debug-androidTest.apk \
