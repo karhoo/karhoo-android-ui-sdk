e4b024d3df8be4f889b25786091ad6e6b7b44a76
Jo Santa Maria <60882714+jsm20@users.noreply.github.com>
2020-12-21 19:02:07 +0000

    Update runner.sh

--- .circleci/runner.sh
+++ .circleci/runner.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 java -jar runner.jar espresso \
    --apikey "$SAUCE_ACCESS_KEY" \
-   --e 'class com.karhoo.uisdk.address.AddressTests' \
    --datacenter https://ondemand.eu-central-1.saucelabs.com \
    --app ../app/build/outputs/apk/debug/app-debug.apk \
    --test ../app/build/outputs/apk/androidTest/debug/app-debug-androidTest.apk \
+   --e 'class com.karhoo.uisdk.address.AddressTests' \
    --device 'deviceNameQuery=Samsung Galaxy S10,platformVersion=10' \
    --help
