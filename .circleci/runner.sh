3e635aa3d95d293aef4a15139a907e92757a7a97
Jo Santa Maria <60882714+jsm20@users.noreply.github.com>
2020-12-21 18:25:04 +0000

    Update to EU

--- .circleci/runner.sh
+++ .circleci/runner.sh
@@ -2,7 +2,7 @@
 java -jar runner.jar espresso \
    --apikey "$SAUCE_ACCESS_KEY" \
    --e 'class com.karhoo.uisdk.address.AddressTests' \
-   --datacenter eu-central-1 \
+   --datacenter EU \
    --app ../app/build/outputs/apk/debug/app-debug.apk \
    --test ../app/build/outputs/apk/androidTest/debug/app-debug-androidTest.apk \
    --device 'deviceNameQuery=Samsung Galaxy S10,platformVersion=10'
