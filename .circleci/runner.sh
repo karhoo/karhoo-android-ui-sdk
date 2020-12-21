b5cc9927373864263283067a84555e2997183cf8
Jo Santa Maria <60882714+jsm20@users.noreply.github.com>
2020-12-21 19:13:16 +0000

    Update deviceNameQuery and platformVersion

--- .circleci/runner.sh
+++ .circleci/runner.sh
@@ -4,6 +4,7 @@ java -jar runner.jar espresso \
    --datacenter https://ondemand.eu-central-1.saucelabs.com \
    --app ../app/build/outputs/apk/debug/app-debug.apk \
    --test ../app/build/outputs/apk/androidTest/debug/app-debug-androidTest.apk \
-   --e 'class com.karhoo.uisdk.address.AddressTests' \
-   --device 'deviceNameQuery=Samsung Galaxy S10,platformVersion=10' \
+   --e class com.karhoo.uisdk.address.AddressTests \
+   --platformVersion=10 \
+   --deviceNameQuery=Samsung Galaxy S10
    --help
