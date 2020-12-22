bcef4b6660f2ae7903da24fd2294bcf293007a7a
Jo Santa Maria <60882714+jsm20@users.noreply.github.com>
2020-12-22 09:32:32 +0000

    Update runner.sh

--- .circleci/runner.sh
+++ .circleci/runner.sh
@@ -4,7 +4,7 @@ java -jar runner.jar espresso \
    --datacenter https://ondemand.eu-central-1.saucelabs.com \
    --app ../app/build/outputs/apk/debug/app-debug.apk \
    --test ../app/build/outputs/apk/androidTest/debug/app-debug-androidTest.apk \
-   --platformVersion="10" \
-   --deviceNameQuery="Samsung Galaxy S10"
+   --platformVersion 10 \
+   --deviceNameQuery Samsung Galaxy S10
    --e class com.karhoo.uisdk.address.AddressTests \
    --help
