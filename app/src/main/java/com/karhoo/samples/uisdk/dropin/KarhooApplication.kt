6914c6e50c4738cf6f7df2f7071fea42c7d01841
Cosmin Badulescu <cosmin.badulescu@karhoo.com>
2021-04-07 15:22:40 +0300

    resolved login method not working

--- app/src/main/java/com/karhoo/samples/uisdk/dropin/KarhooApplication.kt
+++ app/src/main/java/com/karhoo/samples/uisdk/dropin/KarhooApplication.kt
@@ -1,7 +1,8 @@
 package com.karhoo.samples.uisdk.dropin
 
 import android.app.Application
-import com.karhoo.samples.uisdk.dropin.config.GuestConfig
+import com.karhoo.samples.uisdk.dropin.config.KarhooConfig
+import com.karhoo.sdk.api.KarhooApi.setConfiguration
 import com.karhoo.uisdk.KarhooUISDK
 
 class KarhooApplication : Application() {
@@ -9,11 +10,7 @@ class KarhooApplication : Application() {
     override fun onCreate() {
         super.onCreate()
         KarhooUISDK.apply {
-            setConfiguration(
-                GuestConfig(
-                    applicationContext
-                )
-            )
+            setConfiguration(KarhooConfig(applicationContext))
         }
     }
 }
---
+++ app/src/main/java/com/karhoo/samples/uisdk/dropin/config/KarhooConfig.kt
@@ -0,0 +1,44 @@
+package com.karhoo.samples.uisdk.dropin.config
+
+import android.content.Context
+import android.graphics.drawable.Drawable
+import com.karhoo.samples.uisdk.dropin.BuildConfig
+import com.karhoo.samples.uisdk.dropin.R
+import com.karhoo.sdk.analytics.AnalyticProvider
+import com.karhoo.sdk.api.KarhooEnvironment
+import com.karhoo.sdk.api.model.AuthenticationMethod
+import com.karhoo.uisdk.KarhooUISDKConfiguration
+
+class KarhooConfig(val context: Context, private val authMethod: AuthenticationMethod = AuthenticationMethod.KarhooUser()) :
+        KarhooUISDKConfiguration {
+
+    override fun logo(): Drawable? {
+        return context.getDrawable(R.drawable.karhoo_wordmark)
+    }
+
+    override fun environment(): KarhooEnvironment {
+        return if (BuildConfig.BUILD_TYPE == "debug") {
+            KarhooEnvironment.Custom(host = BuildConfig.STAGING_HOST,
+                                     authHost = BuildConfig.STAGING_AUTH_HOST,
+                                     guestHost = BuildConfig.STAGING_GUEST_HOST)
+        } else if (BuildConfig.BUILD_TYPE == "prodQA" || BuildConfig.BUILD_TYPE == "release") {
+            KarhooEnvironment.Production()
+        } else {
+            KarhooEnvironment.Sandbox()
+        }
+    }
+
+    override fun simulatePaymentProvider(): Boolean = false
+
+    override fun context(): Context {
+        return context
+    }
+
+    override fun authenticationMethod(): AuthenticationMethod {
+        return authMethod
+    }
+
+    override fun analyticsProvider(): AnalyticProvider? {
+        return null
+    }
+}
