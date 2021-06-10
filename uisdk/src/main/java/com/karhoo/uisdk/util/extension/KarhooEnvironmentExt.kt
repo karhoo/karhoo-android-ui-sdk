c4409d457dfdcfca4905dbb3bc05f2c2824babf7
Mostafa Hadian <mostafa.hadian@karhoo.com>
2021-06-10 12:37:59 +0100

    Fix lint

--- uisdk/src/main/java/com/karhoo/uisdk/util/extension/KarhooEnvironmentExt.kt
+++ uisdk/src/main/java/com/karhoo/uisdk/util/extension/KarhooEnvironmentExt.kt
@@ -5,11 +5,8 @@ import com.karhoo.uisdk.R
 
 fun KarhooEnvironment.guestTripTrackingUrl(): Int {
     return when (this) {
-        is KarhooEnvironment.Sandbox
-        -> R.string.kh_uisdk_guest_trip_tracking_sandbox
-        is KarhooEnvironment.Production
-        -> R.string.kh_uisdk_guest_trip_tracking_prod
-        else ->
-            R.string.kh_uisdk_guest_trip_tracking_custom
+        is KarhooEnvironment.Sandbox -> R.string.kh_uisdk_guest_trip_tracking_sandbox
+        is KarhooEnvironment.Production -> R.string.kh_uisdk_guest_trip_tracking_production
+        else -> R.string.kh_uisdk_guest_trip_tracking_custom
     }
 }
