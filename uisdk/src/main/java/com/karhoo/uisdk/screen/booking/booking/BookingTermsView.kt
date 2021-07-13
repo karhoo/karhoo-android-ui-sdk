25796879c82f699c9a037a797b18becf5c662ce2
mateid-kr <matei.dediu@karhoo.com>
2021-07-13 10:59:07 +0300

    MOB-3403 Fixed terms and conditions styling

--- uisdk/src/main/java/com/karhoo/uisdk/screen/booking/booking/BookingTermsView.kt
+++ uisdk/src/main/java/com/karhoo/uisdk/screen/booking/booking/BookingTermsView.kt
@@ -16,28 +16,10 @@ class BookingTermsView @JvmOverloads constructor(context: Context,
                                                  defStyleAttr: Int = 0)
     : LinearLayout(context, attrs, defStyleAttr) {
 
-    private var lineTextStyle: Int = R.style.Text_Alternative_Small
-    private var linkTextStyle: Int = R.style.Text_Action_Primary_Small
-
     var actions: PaymentActions? = null
 
     init {
         inflate(context, R.layout.uisdk_view_booking_terms, this)
-        getCustomisationParameters(context, attrs, defStyleAttr)
-    }
-
-    private fun getCustomisationParameters(context: Context, attr: AttributeSet?, defStyleAttr: Int) {
-        val typedArray = context.obtainStyledAttributes(attr, R.styleable.BookingPriceView,
-                                                        defStyleAttr, R.style.KhBookingETAPriceView)
-        lineTextStyle = typedArray.getResourceId(R.styleable.BookingTermsView_termsText, R
-                .style
-                .Text_Alternative_Small)
-        linkTextStyle = typedArray.getResourceId(R.styleable.BookingTermsView_termsLinkText, R
-                .style
-                .Text_Action_Primary_Small)
-        TextViewCompat.setTextAppearance(bookingTermsText, lineTextStyle)
-        TextViewCompat.setTextAppearance(termsConditionsText, linkTextStyle)
-        TextViewCompat.setTextAppearance(cancellationText, linkTextStyle)
     }
 
     fun bindViews(vehicle: Quote) {
