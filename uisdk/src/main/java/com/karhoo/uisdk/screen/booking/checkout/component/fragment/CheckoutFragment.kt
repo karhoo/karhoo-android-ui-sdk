14bcbb8c0690c6e9205d42100585aa85cf0682f4
mateid-kr <matei.dediu@karhoo.com>
2021-09-06 10:13:59 +0300

    MOB-3429 [Passenger Page] As a logged-in user, my informations are pre-populated in all pages in order to speed and ease my completion

--- uisdk/src/main/java/com/karhoo/uisdk/screen/booking/checkout/component/fragment/CheckoutFragment.kt
+++ uisdk/src/main/java/com/karhoo/uisdk/screen/booking/checkout/component/fragment/CheckoutFragment.kt
@@ -22,9 +22,9 @@ internal class CheckoutFragment : Fragment() {
     private lateinit var presenter: CheckoutPresenter
 
     override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
+            inflater: LayoutInflater,
+            container: ViewGroup?,
+            savedInstanceState: Bundle?
                              ): View? {
         val view = inflater.inflate(R.layout.uisdk_booking_checkout_fragment, container, false)
 
@@ -61,10 +61,10 @@ internal class CheckoutFragment : Fragment() {
         }, object : CheckoutFragmentContract.PassengersListener {
             override fun onPassengerPageVisibilityChanged(visible: Boolean) {
                 checkoutActionButton.setText(
-                    presenter.getBookButtonState(
-                        visible, checkoutView
-                            .arePassengerDetailsValid(), checkoutView.isPaymentMethodValid()
-                                                ).resId
+                        presenter.getBookButtonState(
+                                visible, checkoutView
+                                .arePassengerDetailsValid(), checkoutView.isPaymentMethodValid()
+                                                    ).resId
                                             )
             }
 
@@ -74,10 +74,10 @@ internal class CheckoutFragment : Fragment() {
         })
 
         checkoutView.showBookingRequest(
-            quote = bundle.getParcelable(CheckoutActivity.BOOKING_CHECKOUT_QUOTE_KEY)!!,
-            bookingStatus = bundle.getParcelable(CheckoutActivity.BOOKING_CHECKOUT_STATUS_KEY),
-            outboundTripId = bundle.getString(CheckoutActivity.BOOKING_CHECKOUT_OUTBOUND_TRIP_ID_KEY),
-            bookingMetadata = bundle.getSerializable(CheckoutActivity.BOOKING_CHECKOUT_METADATA_KEY) as HashMap<String, String>?
+                quote = bundle.getParcelable(CheckoutActivity.BOOKING_CHECKOUT_QUOTE_KEY)!!,
+                bookingStatus = bundle.getParcelable(CheckoutActivity.BOOKING_CHECKOUT_STATUS_KEY),
+                outboundTripId = bundle.getString(CheckoutActivity.BOOKING_CHECKOUT_OUTBOUND_TRIP_ID_KEY),
+                bookingMetadata = bundle.getSerializable(CheckoutActivity.BOOKING_CHECKOUT_METADATA_KEY) as HashMap<String, String>?
                                        )
 
         checkoutActionButton.actions = object : LoadingButtonView.Actions {
@@ -89,7 +89,8 @@ internal class CheckoutFragment : Fragment() {
                         checkoutActionButton.onLoadingComplete()
                     }
                 } else {
-                    if (KarhooUISDKConfigurationProvider.configuration.authenticationMethod() !is AuthenticationMethod.KarhooUser) {
+                    if (!checkoutView.arePassengerDetailsValid()) {
+                        checkoutView.showPassengerDetails(true)
                         checkoutActionButton.onLoadingComplete()
                     } else {
                         checkoutView.startBooking()
@@ -99,11 +100,11 @@ internal class CheckoutFragment : Fragment() {
         }
 
         checkoutActionButton.setText(
-            presenter.getBookButtonState(
-                arePassengerDetailsValid = checkoutView.arePassengerDetailsValid(),
-                isPaymentValid = checkoutView.isPaymentMethodValid()
-                                        )
-                .resId
+                presenter.getBookButtonState(
+                        arePassengerDetailsValid = checkoutView.arePassengerDetailsValid(),
+                        isPaymentValid = checkoutView.isPaymentMethodValid()
+                                            )
+                        .resId
                                     )
 
         return view
