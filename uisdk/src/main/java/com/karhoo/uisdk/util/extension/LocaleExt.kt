cb0f4d3c189c9a228d96b2a8bd84a8724edd5503
mionescu95 <69837374+mionescu95@users.noreply.github.com>
2021-12-09 10:53:09 +0200

    Update LocaleExt.kt

--- uisdk/src/main/java/com/karhoo/uisdk/util/extension/LocaleExt.kt
+++ uisdk/src/main/java/com/karhoo/uisdk/util/extension/LocaleExt.kt
@@ -4,7 +4,8 @@ import java.util.Locale
 
 fun Locale?.toNormalizedLocale() : String {
     this?.let { locale ->
-        if(locale.toString() in arrayOf("en_US", "en_GB", "fr_FR"))
+        if(locale.toString() in arrayOf("en_US", "en_GB", "fr_FR", "es_ES", "de_DE", "it_IT",
+                                        "nl_NL"))
             return locale.toString().replace('_', '-')
         else
             return "en-GB"
