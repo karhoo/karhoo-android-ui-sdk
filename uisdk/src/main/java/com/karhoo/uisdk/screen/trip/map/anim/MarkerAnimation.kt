3185276da6d43e2d8df3842bc7261a9ce0f0bb46
mionescu95 <69837374+mionescu95@users.noreply.github.com>
2022-03-14 18:59:34 +0200

    MOB-3955  [Accor] Android: Multiple tab on Trip Tracking options causes crash

--- uisdk/src/main/java/com/karhoo/uisdk/screen/trip/map/anim/MarkerAnimation.kt
+++ uisdk/src/main/java/com/karhoo/uisdk/screen/trip/map/anim/MarkerAnimation.kt
@@ -6,6 +6,7 @@ import android.util.Property
 
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.Marker
+import java.lang.Exception
 
 object MarkerAnimation {
 
@@ -13,10 +14,18 @@ object MarkerAnimation {
                         latLngInterpolator: LatLngInterpolator,
                         duration: Long) {
         val typeEvaluator = TypeEvaluator<LatLng> { fraction, startValue, endValue -> latLngInterpolator.interpolate(fraction, startValue, endValue) }
-        val property = Property.of(Marker::class.java, LatLng::class.java, "position")
-        val animator = ObjectAnimator.ofObject(marker, property, typeEvaluator, finalPosition)
-        animator.duration = duration
-        animator.start()
+        try {
+            if((Marker::class.java as Class).getMethod("getPosition") != null){
+                val property = Property.of(Marker::class.java, LatLng::class.java, "position")
+                if(property != null){
+                    val animator = ObjectAnimator.ofObject(marker, property, typeEvaluator, finalPosition)
+                    animator.duration = duration
+                    animator.start()
+                }
+            }
+        }catch (e: Exception){
+            e.printStackTrace()
+        }
     }
 
 }
