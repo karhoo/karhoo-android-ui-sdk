083bf55525bc9a8b1de9052ad9ce06456a899d5f
EdwardW2 <44901854+EdwardW2@users.noreply.github.com>
2022-03-15 22:42:18 +0000

    Add translatable strings

--- uisdk/src/main/java/com/karhoo/uisdk/util/extension/QuoteVehicleExt.kt
+++ uisdk/src/main/java/com/karhoo/uisdk/util/extension/QuoteVehicleExt.kt
@@ -19,6 +19,9 @@ fun QuoteVehicle.categoryToLocalisedString(context: Context): String? {
 fun QuoteVehicle.typeToLocalisedString(context: Context): String? {
     return when (this.vehicleType?.uppercase()) {
         "STANDARD" -> context.getString(R.string.kh_uisdk_vehicle_standard)
+        "MPV" -> context.getString(R.string.kh_uisdk_vehicle_mpv)
+        "BUS" -> context.getString(R.string.kh_uisdk_vehicle_bus)
+        "MOTO" -> context.getString(R.string.kh_uisdk_vehicle_moto)
         else -> vehicleType
     }
 }
