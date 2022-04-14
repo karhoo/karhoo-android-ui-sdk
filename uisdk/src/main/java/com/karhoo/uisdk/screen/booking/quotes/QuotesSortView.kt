b18a4106df2956640142628f790dd5e7bd3ffebe
mionescu95 <69837374+mionescu95@users.noreply.github.com>
2022-04-14 23:32:35 +0300

    retain previously selected

--- uisdk/src/main/java/com/karhoo/uisdk/screen/booking/quotes/QuotesSortView.kt
+++ uisdk/src/main/java/com/karhoo/uisdk/screen/booking/quotes/QuotesSortView.kt
@@ -63,6 +63,7 @@ class QuotesSortView @JvmOverloads constructor(
                 }
             })
         }
+        setSelectedSortMethod(SortMethod.PRICE)
     }
 
     private fun getCustomisationParameters(context: Context, attr: AttributeSet?, defStyleAttr: Int) {
@@ -126,8 +127,6 @@ class QuotesSortView @JvmOverloads constructor(
 
     fun destinationChanged(bookingInfo: BookingInfo?) {
         hasDestination = bookingInfo?.destination != null
-        val sortMethod = if (hasDestination) SortMethod.PRICE else SortMethod.ETA
-        setSelectedSortMethod(sortMethod)
     }
 
     private fun setSortingMethodByTabPosition(position: Int) = if (position == 0) {
