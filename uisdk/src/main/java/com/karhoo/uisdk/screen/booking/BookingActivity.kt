b5788db0a03f2e97599ec1cfc138d30a5c46603c
mateid-kr <79910867+mateid-kr@users.noreply.github.com>
2022-05-17 10:26:18 +0300

    MOB-4104 (#368)
    * MOB-4028 Update UISDK Adyen dependency to the latest version
    
    * MOB-4028 Fix weird lint issue
    
    * MOB-4028 Fix PR comms
    
    * Mob 4005 add track driver option in ride details screen (#349)
    
    * MOB-4005  Add Track Driver option in ride details screen
    
    * too many functions
    
    * Update BookingTermsView.kt (#348)
    
    * Mob 4042 past rides error ksdk05 not showing up any rides (#352)
    
    * MOB-4042  Past Rides error KSDK05 not showing up any rides
    
    * fixed tests
    
    * MOB-4025 Updated NSDK to 1.4.5
    
    * Add potential fix for Accor issue with backgrounded activities
    
    * Cherry pick changes
    
    Co-authored-by: mionescu95 <69837374+mionescu95@users.noreply.github.com>

--- uisdk/src/main/java/com/karhoo/uisdk/screen/booking/BookingActivity.kt
+++ uisdk/src/main/java/com/karhoo/uisdk/screen/booking/BookingActivity.kt
@@ -72,7 +72,7 @@ class BookingActivity : BaseActivity(), AddressBarMVP.Actions, BookingMapMVP.Act
     private var bookingComments: String? = ""
     private var bookingMetadata: HashMap<String, String>? = null
 
-    private var isGuest = KarhooUISDKConfigurationProvider.isGuest()
+    private var isGuest = false
 
     override val layout: Int
         get() = R.layout.uisdk_activity_booking_main
@@ -153,6 +153,8 @@ class BookingActivity : BaseActivity(), AddressBarMVP.Actions, BookingMapMVP.Act
     }
 
     override fun initialiseViews() {
+        isGuest = KarhooUISDKConfigurationProvider.isGuest()
+
         addressBarWidget.watchJourneyDetailsState(this@BookingActivity, journeyDetailsStateViewModel)
         tripAllocationWidget.watchBookingRequestStatus(this@BookingActivity,
                                                        bookingRequestStateViewModel)
