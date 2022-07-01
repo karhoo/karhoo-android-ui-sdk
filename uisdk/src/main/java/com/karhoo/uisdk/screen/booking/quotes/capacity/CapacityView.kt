ea82ecaacc05d2dcc66b1ccae3ec06246c7c7164
mionescu95 <69837374+mionescu95@users.noreply.github.com>
2022-07-01 22:15:19 +0300

    MOB-4036  [Accor-30]Location permission prompt keeps popping

--- uisdk/src/main/java/com/karhoo/uisdk/screen/booking/quotes/capacity/CapacityView.kt
+++ uisdk/src/main/java/com/karhoo/uisdk/screen/booking/quotes/capacity/CapacityView.kt
@@ -16,10 +16,26 @@ class CapacityView @JvmOverloads constructor(
         inflate(context, R.layout.uisdk_view_capacity, this)
     }
 
-    override fun setCapacity(luggage: Int, people: Int, otherCapabilities: Int?) {
-        luggageCapacityText.text =
-            resources.getString(R.string.kh_uisdk_capacity, luggage.toString())
-        peopleCapacityText.text = resources.getString(R.string.kh_uisdk_capacity, people.toString())
+    override fun setCapacity(luggage: Int?, people: Int?, otherCapabilities: Int?) {
+        luggage?.let {
+            luggageCapacityText.text =
+                resources.getString(R.string.kh_uisdk_capacity, luggage.toString())
+            luggageCapacityText.visibility = VISIBLE
+            briefcaseIcon.visibility = VISIBLE
+        }?: kotlin.run {
+            luggageCapacityText.visibility = GONE
+            briefcaseIcon.visibility = GONE
+        }
+
+        people?.let {
+            peopleCapacityText.text = resources.getString(R.string.kh_uisdk_capacity, people.toString())
+            peopleCapacityText.visibility = VISIBLE
+            passengerIcon.visibility = VISIBLE
+        }?: kotlin.run {
+            peopleCapacityText.visibility = GONE
+            passengerIcon.visibility = GONE
+        }
+
 
         val capabilitiesCount = otherCapabilities?.minus(DEFAULT_CAPABILITIES_NO)
 
