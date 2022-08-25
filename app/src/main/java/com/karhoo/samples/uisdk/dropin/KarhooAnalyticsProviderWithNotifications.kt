c548d645af5de698b39567d1721b1f4b5e08025c
Bartlomiej Sopala <bartlomiej.sopala@karhoo.com>
2022-08-25 13:28:18 +0200

    add checkbox for notifications

--- app/src/main/java/com/karhoo/samples/uisdk/dropin/KarhooAnalyticsProviderWithNotifications.kt
+++ app/src/main/java/com/karhoo/samples/uisdk/dropin/KarhooAnalyticsProviderWithNotifications.kt
@@ -1,5 +1,6 @@
 package com.karhoo.samples.uisdk.dropin
 
+import android.content.Context
 import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationManagerCompat
 import com.karhoo.samples.uisdk.dropin.KarhooApplication
@@ -12,14 +13,18 @@ class KarhooAnalyticsProviderWithNotifications: AnalyticProvider {
     }
 
     override fun trackEvent(event: String, payloadMap: Map<String, Any>) {
-        val notification = NotificationCompat.Builder(KarhooApplication.appContext, "analyticsChannel")
-        notification.setContentTitle(event)
-        notification.setContentText(payloadMap.toString())
-        notification.setSmallIcon(R.drawable.uisdk_karhoo_wordmark)
-        notification.setAutoCancel(true)
-        notification.setPriority(NotificationCompat.PRIORITY_MAX)
-        notification.setStyle(NotificationCompat.BigTextStyle().bigText(payloadMap.toString()))
-        val managerCompat = NotificationManagerCompat.from(KarhooApplication.appContext)
-        managerCompat.notify(Random.nextInt(), notification.build())
+        val sharedPrefs = KarhooApplication.appContext.getSharedPreferences("sharedPrefs", Context.MODE_PRIVATE)
+        if (sharedPrefs.getBoolean("notifications_enabled", false)) {
+            val notification =
+                NotificationCompat.Builder(KarhooApplication.appContext, "analyticsChannel")
+            notification.setContentTitle(event)
+            notification.setContentText(payloadMap.toString())
+            notification.setSmallIcon(R.drawable.uisdk_karhoo_wordmark)
+            notification.setAutoCancel(true)
+            notification.setPriority(NotificationCompat.PRIORITY_MAX)
+            notification.setStyle(NotificationCompat.BigTextStyle().bigText(payloadMap.toString()))
+            val managerCompat = NotificationManagerCompat.from(KarhooApplication.appContext)
+            managerCompat.notify(Random.nextInt(), notification.build())
+        }
     }
 }
--- app/src/main/java/com/karhoo/samples/uisdk/dropin/MainActivity.kt
+++ app/src/main/java/com/karhoo/samples/uisdk/dropin/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.karhoo.samples.uisdk.dropin
 
 import android.app.Dialog
+import android.content.Context
 import android.content.Intent
+import android.content.SharedPreferences
 import android.os.Bundle
 import android.view.View
 import android.view.Window
@@ -27,6 +29,7 @@ import com.karhoo.uisdk.screen.booking.checkout.payment.adyen.AdyenPaymentView
 import com.karhoo.uisdk.screen.booking.checkout.payment.braintree.BraintreePaymentView
 import kotlin.system.exitProcess
 import android.util.Log
+import android.widget.CheckBox
 import com.karhoo.farechoice.service.analytics.KarhooAnalytics
 import com.karhoo.sdk.analytics.AnalyticsManager
 import java.util.*
@@ -36,6 +39,8 @@ class MainActivity : AppCompatActivity() {
     private lateinit var loadingProgressBar: View
     private var braintreePaymentManager: BraintreePaymentManager = BraintreePaymentManager()
     private var adyenPaymentManager: AdyenPaymentManager = AdyenPaymentManager()
+    private lateinit var sharedPrefs: SharedPreferences
+    private val notifications_id = "notifications_enabled"
 
     init {
         Thread.setDefaultUncaughtExceptionHandler { _, eh ->
@@ -50,6 +55,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        sharedPrefs = this?.getSharedPreferences("sharedPrefs", Context.MODE_PRIVATE)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
@@ -110,6 +116,9 @@ class MainActivity : AppCompatActivity() {
             }
             showLoginInputDialog()
         }
+
+        val checkbox = findViewById<CheckBox>(R.id.notifications_checkbox)
+        checkbox.setChecked(getCurrentNotificationStatus())
     }
 
     private fun showLoginInputDialog() {
@@ -223,5 +232,24 @@ class MainActivity : AppCompatActivity() {
 
     companion object {
         private val TAG = MainActivity::class.java.name
+    }
+
+    fun onCheckboxClicked(view: View) {view
+        toggleNotificationStatus()
+    }
+
+    private fun getCurrentNotificationStatus() : Boolean {
+         return sharedPrefs.getBoolean(notifications_id, false)
+    }
+
+    private fun setNotificationStatus(value: Boolean){
+        with (sharedPrefs.edit()) {
+            putBoolean(notifications_id, value)
+            apply()
+        }
+    }
+
+    private fun toggleNotificationStatus(){
+        setNotificationStatus(!getCurrentNotificationStatus())
     }
 }
