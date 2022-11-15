a78e521b4f2e7562c09713a0d19f88441ca9c2da
mionescu95 <69837374+mionescu95@users.noreply.github.com>
2022-11-15 09:27:59 +0200

    Create a master bottom view that other types would inherit (#479)
    * Create a master bottom view that other types would inherit
    
    * Update MasterBottomSheetFragment.kt

--- uisdk/src/main/java/com/karhoo/uisdk/screen/booking/quotes/sortview/QuotesSortView.kt
+++ uisdk/src/main/java/com/karhoo/uisdk/screen/booking/quotes/sortview/QuotesSortView.kt
@@ -4,16 +4,14 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageButton
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.Observer
-import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import com.google.android.material.radiobutton.MaterialRadioButton
 import com.karhoo.uisdk.R
-import com.karhoo.uisdk.base.view.LoadingButtonView
+import com.karhoo.uisdk.base.bottomSheet.MasterBottomSheetFragment
 import com.karhoo.uisdk.screen.booking.domain.quotes.SortMethod
 
-class QuotesSortView : BottomSheetDialogFragment() {
+class QuotesSortView : MasterBottomSheetFragment() {
 
     private var listener: Listener? = null
 
@@ -28,12 +26,6 @@ class QuotesSortView : BottomSheetDialogFragment() {
     ): View? {
         val view = inflater.inflate(R.layout.uisdk_view_quotes_sort, container, false)
 
-        val quotesSortBySave = view.findViewById<LoadingButtonView>(R.id.quotesSortBySave)
-        quotesSortBySave.apply {
-            setText(R.string.kh_uisdk_save)
-            setOnClickListener { saveClicked() }
-        }
-
         quotesSortByDriverArrival = view?.findViewById(R.id.quotesSortByDriverArrival)
         quotesSortByPrice = view?.findViewById(R.id.quotesSortByPrice)
 
@@ -45,28 +37,19 @@ class QuotesSortView : BottomSheetDialogFragment() {
         }
         selectedSortMethod.observe(this, nameObserver)
 
-        val quotesSortByCloseDialog = view.findViewById<ImageButton>(R.id.quotesSortByCloseDialog)
-        quotesSortByCloseDialog.apply {
-            setOnClickListener { dismiss() }
-        }
-
         quotesSortByPrice?.apply { setOnClickListener { selectedSortMethod.value = SortMethod.PRICE } }
         quotesSortByDriverArrival?.apply { setOnClickListener { selectedSortMethod.value = SortMethod.ETA } }
+
+        setupHeader(view = view, title = getString(R.string.kh_uisdk_sort_by))
+        setupButton(view = view, buttonId = R.id.quotesSortBySave, text = getString(R.string.kh_uisdk_save)) {
+            selectedSortMethod.value?.let { listener?.onUserChangedSortMethod(it) }
+        }
 
         return view
     }
 
     companion object {
         const val TAG = "QuotesSortView"
-    }
-
-    override fun getTheme(): Int {
-        return R.style.KhQuoteListSortBottomSheetDialogTheme
-    }
-
-    private fun saveClicked(){
-        selectedSortMethod.value?.let { listener?.onUserChangedSortMethod(it) }
-        dismiss()
     }
 
     interface Listener {
