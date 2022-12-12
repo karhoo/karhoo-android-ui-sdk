eb6308b8d4f51b89b788a5ab0eded00e6d4458bd
mionescu95 <69837374+mionescu95@users.noreply.github.com>
2022-12-12 15:57:46 +0200

    Braintree removal (#498)
    * maybe removed braintree
    
    * Update settings.gradle
    
    * Update build.gradle
    
    * revert
    
    * Update settings.gradle
    
    * Update build.gradle
    
    * removed braintree
    
    * added marginBottom dimen
    
    * commented braintree tests
    
    * Update build.gradle
    
    * Update AndroidManifest.xml

--- app/src/main/java/com/karhoo/samples/uisdk/dropin/MainActivity.kt
+++ app/src/main/java/com/karhoo/samples/uisdk/dropin/MainActivity.kt
@@ -26,7 +26,6 @@ import com.karhoo.uisdk.screen.booking.BookingActivity
 import com.karhoo.uisdk.screen.booking.checkout.payment.AdyenPaymentManager
 import com.karhoo.uisdk.screen.booking.checkout.payment.BraintreePaymentManager
 import com.karhoo.uisdk.screen.booking.checkout.payment.adyen.AdyenPaymentView
-import com.karhoo.uisdk.screen.booking.checkout.payment.braintree.BraintreePaymentView
 import kotlin.system.exitProcess
 import kotlinx.coroutines.GlobalScope
 import android.util.Log
@@ -72,7 +71,6 @@ class MainActivity : AppCompatActivity() {
 
         KarhooApi.paymentsService.getAdyenClientKey()
         adyenPaymentManager.paymentProviderView = AdyenPaymentView()
-        braintreePaymentManager.paymentProviderView = BraintreePaymentView()
 
         loadingProgressBar = findViewById<View>(R.id.loadingSpinner)
 
