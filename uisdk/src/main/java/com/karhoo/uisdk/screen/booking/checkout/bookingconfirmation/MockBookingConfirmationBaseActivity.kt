5296f252506adcce25e104f744212fecca960fd6
mionescu95 <69837374+mionescu95@users.noreply.github.com>
2022-12-19 11:04:25 +0200

    uisdk_mock_booking_confirmation_layout

--- uisdk/src/main/java/com/karhoo/uisdk/screen/booking/checkout/bookingconfirmation/MockBookingConfirmationBaseActivity.kt
+++ uisdk/src/main/java/com/karhoo/uisdk/screen/booking/checkout/bookingconfirmation/MockBookingConfirmationBaseActivity.kt
@@ -1,13 +1,13 @@
-package com.karhoo.uisdk.booking.checkout.bookingconfirmation
+package com.karhoo.uisdk.screen.booking.checkout.bookingconfirmation
 
+import com.karhoo.uisdk.R
 import com.karhoo.uisdk.base.BaseActivity
-import com.karhoo.uisdk.screen.booking.checkout.bookingconfirmation.BookingConfirmationView
 import com.karhoo.uisdk.screen.booking.checkout.component.views.CheckoutViewContract
 import com.karhoo.uisdk.screen.booking.checkout.loyalty.LoyaltyMode
 
 class MockBookingConfirmationBaseActivity : BaseActivity() {
     override val layout: Int
-        get() = com.karhoo.uisdk.R.layout.uisdk_mock_booking_confirmation_layout
+        get() = R.layout.uisdk_mock_booking_confirmation_layout
 
     override fun handleExtras() {
         val bookingConfirmationView = BookingConfirmationView(
