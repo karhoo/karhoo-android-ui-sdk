6d8e444dbb4532b5c0136826602c7537235749f8
mateid-kr <matei.dediu@karhoo.com>
2023-01-18 16:54:26 +0200

    MOB-4421 Removed capabilities commented code

--- uisdk/src/main/java/com/karhoo/uisdk/screen/booking/quotes/capacity/CapacityView.kt
+++ uisdk/src/main/java/com/karhoo/uisdk/screen/booking/quotes/capacity/CapacityView.kt
@@ -35,17 +35,6 @@ class CapacityView @JvmOverloads constructor(
             peopleCapacityText.visibility = GONE
             passengerIcon.visibility = GONE
         }
-
-
-//        val capabilitiesCount = otherCapabilities?.minus(DEFAULT_CAPABILITIES_NO)
-//
-//        if (capabilitiesCount != null && capabilitiesCount > 0) {
-//            otherCapabilitiesText.text =
-//                resources.getString(R.string.kh_uisdk_extra_capabilities, capabilitiesCount)
-//            otherCapabilitiesText.visibility = VISIBLE
-//        } else {
-//            otherCapabilitiesText.visibility = GONE
-//        }
     }
 
     override fun showCapacities(show: Boolean) {
