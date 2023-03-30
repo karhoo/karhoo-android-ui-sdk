3b6b1a4ef02360e482afbaf81a31472bfda4d8cc
mionescu95 <69837374+mionescu95@users.noreply.github.com>
2023-03-30 07:44:42 +0300

    MOB-4626 Make Legal notice accordeon always open on Android

--- uisdk/src/main/java/com/karhoo/uisdk/screen/booking/checkout/legalnotice/LegalNoticeView.kt
+++ uisdk/src/main/java/com/karhoo/uisdk/screen/booking/checkout/legalnotice/LegalNoticeView.kt
@@ -3,21 +3,16 @@ package com.karhoo.uisdk.screen.booking.checkout.legalnotice
 import android.content.Context
 import android.graphics.drawable.Drawable
 import android.util.AttributeSet
-import android.view.View
-import android.view.animation.Animation
-import android.view.animation.AnimationUtils
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.content.res.ResourcesCompat
 import com.karhoo.uisdk.R
 import com.karhoo.uisdk.screen.booking.checkout.payment.WebViewActions
 import kotlinx.android.synthetic.main.uisdk_view_legal_notice.view.legalNoticeContainer
-import kotlinx.android.synthetic.main.uisdk_view_legal_notice.view.legalNoticeIcon
 
 import android.net.Uri
 import android.content.Intent
 import android.text.method.LinkMovementMethod
 import com.karhoo.uisdk.screen.booking.domain.support.KarhooFeedbackEmailComposer
-import kotlinx.android.synthetic.main.uisdk_view_legal_notice.view.legalNoticeLabelContainer
 import kotlinx.android.synthetic.main.uisdk_view_legal_notice.view.legalNoticeText
 
 class LegalNoticeView @JvmOverloads constructor(
@@ -29,7 +24,6 @@ class LegalNoticeView @JvmOverloads constructor(
 
     var actions: WebViewActions? = null
     private var presenter: LegalNoticePresenter
-    private var isExpanded: Boolean = false
     private lateinit var emailComposer: KarhooFeedbackEmailComposer
 
     init {
@@ -40,18 +34,6 @@ class LegalNoticeView @JvmOverloads constructor(
         presenter.attachView(this)
 
         emailComposer = KarhooFeedbackEmailComposer(context)
-
-        isExpanded = resources.getString(R.string.kh_uisdk_legal_notice_text).isNotEmpty()
-
-        legalNoticeIcon.setImageDrawable(getArrowIcon())
-
-        legalNoticeLabelContainer.setOnClickListener {
-            isExpanded = !isExpanded
-
-            legalNoticeIcon.setImageDrawable(getArrowIcon())
-
-            expandLegalNoticeSection(isExpanded, legalNoticeText)
-        }
 
         bindView()
     }
@@ -71,38 +53,6 @@ class LegalNoticeView @JvmOverloads constructor(
         }
     }
 
-    override fun expandLegalNoticeSection(expand: Boolean, view: View) {
-        val animationType = if (expand) {
-            R.anim.kh_uisdk_fade_in
-        } else {
-            R.anim.kh_uisdk_fade_out
-        }
-
-        val animation = AnimationUtils.loadAnimation(context, animationType)
-
-        animation.setAnimationListener(object : Animation.AnimationListener {
-            override fun onAnimationStart(animation: Animation?) {
-                if (expand) {
-                    view.visibility = VISIBLE
-                }
-            }
-
-            override fun onAnimationRepeat(animation: Animation?) {
-                //do nothing
-            }
-
-            override fun onAnimationEnd(animation: Animation?) {
-                view.clearAnimation()
-
-                if (!expand) {
-                    view.visibility = GONE
-                }
-            }
-        })
-
-        view.startAnimation(animation)
-    }
-
     override fun getDrawableResource(id: Int): Drawable? {
         return ResourcesCompat.getDrawable(resources, id, null)
     }
@@ -117,13 +67,6 @@ class LegalNoticeView @JvmOverloads constructor(
         intent?.let {
             context.startActivity(it)
         }
-    }
-
-    private fun getArrowIcon(): Drawable? {
-        return if (isExpanded)
-            getDrawableResource(R.drawable.kh_uisdk_ic_arrow_up_small)
-        else
-            getDrawableResource(R.drawable.kh_uisdk_ic_arrow_down_small)
     }
 
     companion object {
