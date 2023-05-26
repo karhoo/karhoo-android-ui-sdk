97c50d566ac5d421b2e3fb0c47576cf4ec7ed9e1
Diana Petrea <diana.petrea@karhoo.com>
2023-05-26 17:17:23 +0300

    remove nesting layer

--- uisdk/src/main/java/com/karhoo/uisdk/util/extension/StringExt.kt
+++ uisdk/src/main/java/com/karhoo/uisdk/util/extension/StringExt.kt
@@ -2,30 +2,30 @@ package com.karhoo.uisdk.util.extension
 
 fun String?.removeLastOccurrenceOf(substring: String?, caseInsensitive: Boolean = true, prettify: Boolean = true): String {
     this.let { string ->
-        if (string != null) {
-            if (substring == null || !string.lowercase().contains(substring.lowercase())) {
-                return string
-            }
-
-            try {
-                // Replace the last occurrence of the substring
-                val pattern = "(\\b".plus(substring).plus("\\b)(?!.*\\1)")
-                // Take into account the case sensitivity preference
-                val regex = if (caseInsensitive) Regex(pattern, RegexOption.IGNORE_CASE) else Regex(pattern)
-                var result = string.replace(regex, "")
-
-                if (prettify) {
-                    result = result.removeSubstringWithRegexUsing(" +,{1} +", " ")
-                }
-                return result
-            }
-            catch (e: java.lang.Exception) {
-                println(e)
-                return string
-            }
+        if (string == null) {
+            return ""
         }
 
-        return ""
+        if (substring == null || !string.lowercase().contains(substring.lowercase())) {
+            return string
+        }
+
+        try {
+            // Replace the last occurrence of the substring
+            val pattern = "(\\b".plus(substring).plus("\\b)(?!.*\\1)")
+            // Take into account the case sensitivity preference
+            val regex = if (caseInsensitive) Regex(pattern, RegexOption.IGNORE_CASE) else Regex(pattern)
+            var result = string.replace(regex, "")
+
+            if (prettify) {
+                result = result.removeSubstringWithRegexUsing(" +,{1} +", " ")
+            }
+            return result
+        }
+        catch (e: java.lang.Exception) {
+            println(e)
+            return string
+        }
     }
 }
 
