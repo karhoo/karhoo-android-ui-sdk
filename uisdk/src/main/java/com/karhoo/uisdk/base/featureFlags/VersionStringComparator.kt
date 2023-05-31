a342d6658a999f4ffd7d67d02ad524d783ef740e
Bartłomiej Sopala <bartlomiej.sopala@karhoo.com>
2023-05-31 23:05:58 +0200

    fix comparator, all text are passing

--- uisdk/src/main/java/com/karhoo/uisdk/base/featureFlags/VersionStringComparator.kt
+++ uisdk/src/main/java/com/karhoo/uisdk/base/featureFlags/VersionStringComparator.kt
@@ -4,63 +4,25 @@ import java.util.Comparator
 
 class VersionStringComparator : Comparator<String> {
 
-    // Compares two Strings
-    private fun check(a: String, b: String): Int {
-        val al = a.length
-        val bl = b.length
-
-        var i = 0
-        var j = 0
-        while (i < al && j < bl) {
-            if (a[i] == b[j]) {
-                i++
-                j++
-            } else if (a[i] > b[j]) {
-                return 1
-            } else {
-                return -1
-            }
-        }
-
-        if (i == al && j == bl) {
-            return 0
-        }
-        if (i == al) {
-            return -1
-        }
-        return 1
+    private fun getVersionsComponents(a: String): List<Int> {
+        return a.split(".").toTypedArray().map { it.toInt() }
     }
 
-    // Function to split Strings based on dots
-    private fun getTokens(a: String): Array<String> {
-        return a.split(".").toTypedArray()
-    }
-
-    // Comparator to sort the array of Strings
     override fun compare(a: String, b: String): Int {
-
         // Stores the numerical subStrings
-        val va = getTokens(a)
-        val vb = getTokens(b)
+        val va = getVersionsComponents(a)
+        val vb = getVersionsComponents(b)
 
-        // Iterate up to length of minimum
-        // of the two Strings
         for (i in 0 until Math.min(va.size, vb.size)) {
-
-            // Compare each numerical subString
-            // of the two Strings
-            val countCheck = check(va[i], vb[i])
-
-            if (countCheck == -1) {
-                return -1
-            } else if (countCheck == 1) {
+            if (va[i] > vb[i]) {
                 return 1
+            } else if (va[i] < vb[i]) {
+                return -1
             }
         }
-
         return when {
             va.size < vb.size -> -1
-            else -> 1
+            else -> 0
         }
     }
 }
