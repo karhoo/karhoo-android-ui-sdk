ef7ac685419f59200a613ef7f7dd74aabb0af9dc
mionescu95 <69837374+mionescu95@users.noreply.github.com>
2023-07-21 17:08:37 +0300

    Release 1.13.1 hotfix feature flags network check

--- uisdk/src/main/java/com/karhoo/uisdk/base/featureFlags/FeatureFlagsService.kt
+++ uisdk/src/main/java/com/karhoo/uisdk/base/featureFlags/FeatureFlagsService.kt
@@ -1,34 +1,86 @@
 package com.karhoo.uisdk.base.featureFlags
 
 import android.content.Context
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
+import android.os.Build
 import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
 import com.karhoo.uisdk.BuildConfig
 import com.karhoo.uisdk.base.FeatureFlagsModel
+import java.lang.Exception
 import java.net.URL
 import javax.net.ssl.HttpsURLConnection
 import kotlin.concurrent.thread
 
-class FeatureFlagsService(context: Context, private val featureFlagsStore: FeatureFlagsStore = KarhooFeatureFlagsStore(context)) {
+class FeatureFlagsService(
+    private val context: Context,
+    private val featureFlagsStore: FeatureFlagsStore = KarhooFeatureFlagsStore(
+        context
+    )
+) {
 
     fun update() {
-        thread {
-            val jsonUrl = "https://raw.githubusercontent.com/karhoo/karhoo-android-ui-sdk/master/feature_flag.json"
-
-            val url = URL(jsonUrl)
-            val connection = url.openConnection() as HttpsURLConnection
-            val inputStream = connection.inputStream
-
-            val data = inputStream.readBytes()
-            val jsonString = String(data)
-
-            val type = object : TypeToken<List<FeatureFlagsModel>>() {
-
-            }.type
-            val decoder = Gson().fromJson<List<FeatureFlagsModel>>(jsonString, type)
-
-            handleFlagSets(decoder)
+        try {
+            if (!isNetworkAvailable(context))
+                return
+        }catch (e: Exception){
+            e.printStackTrace()
         }
+
+        thread {
+            val jsonUrl =
+                "https://raw.githubusercontent.com/karhoo/karhoo-android-ui-sdk/master/feature_flag.json"
+
+            try {
+                val url = URL(jsonUrl)
+                val connection = url.openConnection() as HttpsURLConnection
+                val inputStream = connection.inputStream
+
+                val data = inputStream.readBytes()
+                val jsonString = String(data)
+
+                val type = object : TypeToken<List<FeatureFlagsModel>>() {
+
+                }.type
+                val decoder = Gson().fromJson<List<FeatureFlagsModel>>(jsonString, type)
+
+                handleFlagSets(decoder)
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+        }
+    }
+
+    private fun isNetworkAvailable(context: Context?): Boolean {
+        if (context == null) return false
+        val connectivityManager =
+            context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            val capabilities =
+                connectivityManager.getNetworkCapabilities(connectivityManager.activeNetwork)
+            if (capabilities != null) {
+                when {
+                    capabilities.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> {
+                        return true
+                    }
+
+                    capabilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> {
+                        return true
+                    }
+
+                    capabilities.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET) -> {
+                        return true
+                    }
+                }
+            }
+        } else {
+            val activeNetworkInfo = connectivityManager.activeNetworkInfo
+            if (activeNetworkInfo != null && activeNetworkInfo.isConnected) {
+                return true
+            }
+        }
+        return false
     }
 
     fun handleFlagSets(sets: List<FeatureFlagsModel>) {
@@ -36,12 +88,15 @@ class FeatureFlagsService(context: Context, private val featureFlagsStore: Featu
         storeFeatureFlag(selectedSet)
     }
 
-    private fun selectProperSet(version: String, sets: List<FeatureFlagsModel>): FeatureFlagsModel? {
+    private fun selectProperSet(
+        version: String,
+        sets: List<FeatureFlagsModel>
+    ): FeatureFlagsModel? {
         val versionStringComparator = VersionStringComparator()
         val sortedSets = sets.sortedWith(FeatureFlagsVersionComparator()).reversed()
 
         for (set in sortedSets) {
-            if ( versionStringComparator.compare(set.version, version) <= 0) {
+            if (versionStringComparator.compare(set.version, version) <= 0) {
                 return set
             }
 
